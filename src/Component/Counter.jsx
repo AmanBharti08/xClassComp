f36a33e778070aeebb5ea90a0822bb4097c06362
AmanBharti08 <amanbharti0897@gmail.com>
2024-09-13 20:03:30 +0530

    commit

---
+++ src/Component/Counter.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handledecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handledecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
